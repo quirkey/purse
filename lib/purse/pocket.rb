470dc93aaa22542763ae3c427e44934ffe850b01
Aaron Quint <aaron@quirkey.com>
2008-08-07 23:39:46 -0400

    Basic implementation of Pocket (without git support)

--- lib/purse/pocket.rb
+++ lib/purse/pocket.rb
@@ -1,5 +1,42 @@
 module Purse
   class Pocket
+    attr_reader :path
+    
+    def initialize(path)
+      Purse.check_for_parameter('path', path)
+      @path = File.expand_path(path)
+    end
+    
+    def find(name)
+      Purse.check_for_parameter('name', name)
+      note = notes.find {|note| note.name == name }
+      note ? note : raise(Purse::MissingFile, "Could note find the note named #{note}")
+    end
+    
+    def notes
+      @notes ||= load_notes
+    end
+    
+    def notes_paths
+      Dir[File.join(@path, '*.note')]
+    end
+    
+    def load_notes
+      notes_paths.collect {|note_path| Note.load(File.dirname(note_path), File.basename(note_path, '.note'))}
+    end
+    
+    def re_encrypt(password)
+      Purse.check_for_parameter('password', password)
+      notes.collect {|n| n.save(password) }
+    end
+    
+    def push
+      
+    end
+    
+    def pull
+      
+    end
     
   end
 end
--- test/test_purse_pocket.rb
+++ test/test_purse_pocket.rb
@@ -1,62 +1,115 @@
 require 'test_helper.rb'
 
 class TestPursePocket < Test::Unit::TestCase
-  
+
   context "Purse" do
     context "Pocket" do
-      
+
       context "an instance" do
         setup do
+          @pocket = Pocket.new(purse_path)
         end
-        
+
         should "require a path" do
           assert_raise(MissingParameter) do
             Pocket.new(nil)
           end
         end
-        
-        context "initialize" do
-          should "assign path to @path" do
 
+        context "initialize" do
+
+          should "assign path to @path" do
+            assert purse_path, @pocket.path
+          end
+
+        end
+        context "notes" do
+
+          setup do
+            @notes = @pocket.notes
           end
 
           should "find notes by Dir[]" do
-
-          end
-
-          should "load all notes in path to @notes" do
-
+            assert Dir[purse_path + '/*.note'], @pocket.notes_paths
           end
 
           should "assign notes to an array in @notes" do
-
+            assert_set_of Note, @notes
           end
         end
-        
+
         context "find" do
-          
-          context "with a loaded note name" do
-            should "return the note" do
+          setup do
+            Dir.expects(:[]).at_most_once.returns([File.join(purse_path, 'jagger.note')])
+            @notes = @pocket.notes
+          end
 
+          context "with a loaded note name" do
+            setup do
+              @note = @pocket.find('jagger')
+            end
+
+            should "return the note" do
+              assert @note.is_a?(Note)
+              assert_equal 'jagger', @note.name
             end
           end
-          
-          context "with an unknown note" do
+
+          context "with an unknown note" do            
             should "raise error" do
-              
+              assert_raise(MissingFile) do
+                @pocket.find('bloober')
+              end
             end
           end
         end
-        
+
         context "re_encrypt" do
-          should_eventually "load all files and re-encrpyt them with a new password" do
-            
+          should "require a password" do
+            assert_raise(MissingParameter) do
+              @pocket.re_encrypt(nil)
+            end
           end
+
+          context "with a password" do            
+            should "call save on each note" do
+              password = 'newpass'
+              Note.any_instance.stubs(:save).times(3).with(password)
+              @pocket.re_encrypt(password)
+            end
+          end
+
         end
-        
+
+        context "push" do
+          context "if there is already a git directory" do
+
+          end
+
+          context "if there is no git remote setting" do
+
+          end
+
+          context "if there is a remote setting but there is no repository" do
+
+          end
+
+          should_eventually "push to git repo" do
+
+          end
+
+        end
+
+        context "pull" do
+
+          should_eventually "pull from a git server" do
+
+          end
+
+        end
       end
     end
   end
-  
+
 
 end
